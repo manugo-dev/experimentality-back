3b66a339f1905cc3d930cb94110da72aac896893
manuelgomezo <manuel.gomezo@udea.edu.co>
2019-10-09 11:31:23 -0500

    Some prettier and ESLint config changed

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 /* Homepage. */
 router.get('/:name?', (req, res) => {
-  res.send(`Hi ${req.query.name ? req.query.name : 'friend'}, Welcome to Experimentality.`);
+  res.send(
+    `Hi ${
+      req.query.name ? req.query.name : 'friend'
+    }, Welcome to Experimentality.`,
+  );
 });
 
 export default router;
