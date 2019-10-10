8cc41c31706f50dc01817c69a8e639be3d93295b
manuelgomezo <manuel.gomezo@udea.edu.co>
2019-10-09 19:24:07 -0500

    Firebase Auth implementation with minimal website

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import checkIfAuthenticated from '../middlewares/firebase-auth';
 
 const router = express.Router();
 
@@ -9,6 +10,21 @@ router.get('/:name?', (req, res) => {
       req.query.name ? req.query.name : 'friend'
     }, Welcome to Experimentality.`,
   );
+});
+
+// Remove quote endpoint
+router.get('/allQuotes', async (req, res) => {
+  return res.send(`El que quieres eliminar es el id ${req.body.id}`);
+});
+
+// Edit quote endpoint
+router.put('/editQuote', async (req, res) => {
+  return res.send(req.body.id);
+});
+
+// Remove quote endpoint
+router.delete('/removeQuote', checkIfAuthenticated, async (req, res) => {
+  return res.send(`El que quieres eliminar es el id ${req.body.id}`);
 });
 
 export default router;
