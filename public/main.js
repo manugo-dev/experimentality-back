b51bfd7e449fded7982df04d72906ee00256e2e5
manuelgomezo <manuel.gomezo@udea.edu.co>
2019-10-11 11:24:52 -0500

    Basic front for Backend and some corrections to route Get Quote

---
+++ public/main.js
@@ -0,0 +1,89 @@
+/* eslint-disable  */
+const server = 'https://experimentality-back.herokuapp.com';
+
+const generateQuote = async () => {
+  const response = await axios({
+    method: 'GET',
+    url: `${server}/api/v1/generate-changing-life-quote`,
+    params: {
+      id: this.id,
+    },
+    data: {},
+  }).catch(error => {
+    throw new Error(error);
+  });
+  return response.data;
+};
+
+const getAllQuotes = async id => {
+  const response = await axios({
+    method: 'GET',
+    url: `${server}/api/v1/all`,
+    data: {},
+  }).catch(error => {
+    throw new Error(error);
+  });
+  return response.data;
+};
+
+const getQuote = async reference => {
+  const response = await axios({
+    method: 'GET',
+    url: `${server}/api/v1/quote/${reference}`,
+  }).catch(error => {
+    toastr.error(error);
+    throw new Error(error);
+  });
+  toastr.success('Quote obtained');
+  return response.data;
+};
+
+const removeQuote = async reference => {
+  if (!bearerToken) {
+    toastr.error('Not authenticated');
+    return;
+  }
+  axios({
+    method: 'DELETE',
+    url: `${server}/api/v1/remove-quote/${reference}`,
+    headers: {
+      Authorization: `bearer ${bearerToken}`,
+    },
+  })
+    .then(data => {
+      toastr.success(`Removed: ${data}`);
+    })
+    .catch(error => {
+      toastr.error(error);
+      throw new Error(error);
+    });
+};
+
+const renderQuote = async data => {
+  const { id, quote, image } = data;
+  document.querySelector('.quote__id').innerText = `ID: ${id}`;
+  document.querySelector('.quote__text').innerText = quote;
+  document.querySelector('.quote__image').src = image;
+};
+
+const generateNewQuote = async () => {
+  generateQuote().then(data => {
+    renderQuote(data);
+  });
+};
+
+const renderIdQuote = async id => {
+  getQuote(id).then(data => {
+    renderQuote(data);
+  });
+};
+
+const toggleDiv = className =>
+  document.querySelector(className).classList.toggle('showDiv');
+
+this.window.onload = () => {
+  generateQuote().then(data => {
+    renderQuote(data);
+    toggleDiv('.content__quote');
+  });
+};
